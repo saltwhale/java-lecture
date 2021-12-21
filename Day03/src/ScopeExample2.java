8f47940fda21e16b1dedab3988e96cb9db9d02a8
소금 <salt_whale@naver.com>
2021-12-21 21:41:00 +0900

    변수 범위 연습

--- Day03/src/ScopeExample2.java
+++ Day03/src/ScopeExample2.java
@@ -5,14 +5,16 @@ public class ScopeExample2 {
 	static String name = "너는 바보";
 	
 	public static void main(String[] args) {
+		// 선언된 변수는 중괄호 기준으로 범위가 정해진다.
 		String name = "너";
-		System.out.println(name);
+		System.out.println(name); //너
 		printName();
 		
 	}
 	
 	public static void printName() {
-		System.out.println(name);
+		// 사용할 변수명이 없으면, 자기 상위 영역 중괄호를 체크한다.
+		System.out.println(name); //너는 바보
 		
 	}
 
