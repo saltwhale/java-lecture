9d62857b16985e826f8d0a32d823f2628bd6e9ee
소금 <salt_whale@naver.com>
2021-12-23 21:23:49 +0900

    삼항연산 연습

---
+++ Day04/src/TExample.java
@@ -0,0 +1,15 @@
+
+public class TExample {
+
+	public static void main(String[] args) {
+		
+		// 삼항연산
+		// 1. 항은 3개
+		// (비교문) ? 비교문이 참 : 비교문이 거짓; 
+		
+		int a = 10, b = 20;
+		int c = (a > b) ? 100 : -100;
+		System.out.println(c);
+		
+	}
+}
