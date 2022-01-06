5870a7cb1749bec379803fc7a114ce4a1e9bdacc
소금 <salt_whale@naver.com>
2022-01-06 21:58:58 +0900

    싱글톤 연습

---
+++ Day09/src/AttExample.java
@@ -0,0 +1,27 @@
+import student.*;
+
+public class AttExample {
+
+	public static void main(String[] args) {
+		
+		//일반 객체생성
+		EleStudent ele1 = new EleStudent();
+		ele1.name = "보노보노";
+		ele1.printName();
+		
+		EleStudent ele2 = new EleStudent();
+		ele2.printName();
+		
+		// 싱글턴 패턴
+		System.out.println("싱글턴 패턴");
+		EleStudent ele3 = EleStudent.getInstance();
+		ele3.name = "포로리";
+		ele3.printName();
+		EleStudent ele4 = EleStudent.getInstance();
+		ele4.printName();
+		EleStudent ele5 = EleStudent.getInstance();
+		ele5.printName();
+		
+	}
+	
+}
--- Day09/src/PackageExample.java
+++ Day09/src/PackageExample.java
@@ -1,4 +1,19 @@
+import student.EleStudent;
+//student패키지 폴더의 EleStudent 라는 클래스만 사용한다.
+
+//import student.*;(* == all)
+//student 패키지/폴더에 있는 모든 클래스들을 참조할 수 있다.
 
 public class PackageExample {
 
+	public static void main(String[] args) {
+		
+		//student.EleStudent ele = student.EleStudent();
+		//import 없이 모든 코드에 패키지/폴더명을 명시해서 사용할 수 있다.
+		EleStudent ele = new EleStudent();
+		ele.name = "보노보노";
+		ele.grade = 4;
+		
+	}
+	
 }
