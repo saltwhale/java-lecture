6bf4dd8680ae8bb1b4298908303e4ec0007086c2
소금 <salt_whale@naver.com>
2022-01-07 21:56:33 +0900

    내부클래스 연습(수정)

--- Day10/src/InnerClass.java
+++ Day10/src/InnerClass.java
@@ -1,4 +1,20 @@
 
 public class InnerClass {
 
+	class InnerInnerClass{	//①객체생성을 해야 사용할 수 있는 이너클래스
+		
+	}
+	
+	static class StaticInnerClass { //②static 이너클래스
+		
+	}
+	
+	public static void main(String[] args) {
+		InnerClass cls = new InnerClass();
+		InnerClass.InnerInnerClass inCls = cls.new InnerInnerClass(); //①객체.new 이너클래스
+		InnerClass.StaticInnerClass sCls = new InnerClass.StaticInnerClass(); //②new 이너클래스
+		
+	}
 }
+
+//이너클래스는 상속을 받지 않더라도 상위 클래스에 접근할 수 있다.
